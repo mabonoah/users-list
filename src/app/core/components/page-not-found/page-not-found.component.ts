c3e00d0e7279dbbd5f1a8a28fa0336dc67e610dc
muhammad-a-ali <m.abdulati.ali@gmail.com>
2020-09-13 01:35:49 +0200

    page not found component

--- src/app/core/components/page-not-found/page-not-found.component.ts
+++ src/app/core/components/page-not-found/page-not-found.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { Location } from '@angular/common';
 
 @Component({
-  selector: 'app-page-not-found',
+  selector: 'page-not-found',
   templateUrl: './page-not-found.component.html',
   styleUrls: ['./page-not-found.component.scss']
 })
 export class PageNotFoundComponent implements OnInit {
 
-  constructor() { }
+  constructor(private location: Location) { }
 
   ngOnInit(): void {
+  }
+
+  /** to go back to the previous page */
+  goBack() {
+    this.location.back();
   }
 
 }
